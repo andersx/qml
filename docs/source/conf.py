62d6c4c38cfe7d285b8b4d9736ca2899e03dadfc
andersx <andersx@nano.ku.dk>
2017-06-04 15:53:31 +0200

    updated path in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../qml'))
+sys.path.insert(0, os.path.abspath('../src'))
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
